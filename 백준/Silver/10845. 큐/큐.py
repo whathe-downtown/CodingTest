4170691526c81821a4761264b261fd5798593162
Chungkyu Yu <70245821+whathe-downtown@users.noreply.github.com>
2022-05-18 13:42:51 +0900

    [Silver IV] Title: 큐, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,31 +1,35 @@
 import sys
 
-N = int(sys.stdin.readline())
 
-queue = []
+num = int(input())
+stack = []
+for i in range (num):
+    command = sys.stdin.readline().split()
 
-for i in range(N):
-    cmd = sys.stdin.readline().split()
+    if command[0] =='push':
+        stack.append(command[1])
 
-    if cmd[0] == "push":
-        queue.insert(0, cmd[1])
-        ##print(queue)
+    elif command[0] =='front':
+        if len(stack) <1 :
+            print(-1)
+        else : print(stack[0])
 
-    elif cmd[0] == "pop":
-        if len(queue) != 0: print(queue.pop())
-        else: print(-1)
+    elif command[0] =='back':
+        if len(stack) <1 :
+            print(-1)
+        else : print(stack[-1])
 
-    elif cmd[0] == "size":
-        print(len(queue))
+    elif command[0] == 'size':
+        print(len(stack))
 
-    elif cmd[0] == "empty":
-        if len(queue) == 0: print(1)
-        else : print(0)
+    elif command[0] =='pop':
+        if len(stack) <1 :
+            print(-1)
+        else:
+            print(stack.pop(0))
 
-    elif cmd[0] == "front":
-        if len(queue) == 0: print(-1)
-        else: print(queue[len(queue) -1])
-
-    elif cmd[0] == "back":
-        if len(queue) == 0: print(-1)
-        else: print(queue[0])
+    elif command[0] == 'empty':
+        if len(stack) < 1:
+            print(1)
+        else:
+            print(0)
