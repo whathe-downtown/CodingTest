1cda13a87d64aaed7a387040b1fae95b92fc00bc
Chungkyu Yu <70245821+whathe-downtown@users.noreply.github.com>
2022-06-12 16:20:35 +0900

    [Silver V] Title: 셀프 넘버, Time: 80 ms, Memory: 31352 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -0,0 +1,11 @@
+num = set(range(1,10001))
+gen_num = set()
+
+for i in range(1,10001):
+    for j in str(i):
+        i += int(j)
+    gen_num.add(i)
+
+self_num = sorted(num - gen_num)
+for i in self_num:
+    print(i)
