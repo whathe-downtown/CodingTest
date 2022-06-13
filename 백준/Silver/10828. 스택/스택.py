1911384924d89e74718c1d0c57ff9113e7651ffc
Chungkyu Yu <70245821+whathe-downtown@users.noreply.github.com>
2022-06-13 13:43:00 +0900

    [Silver IV] Title: 스택, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,29 +1,29 @@
-import sys
-num = int(sys.stdin.readline())
+from sys import stdin
 
+num = int(input())
 stack = []
 for i in range(num):
-    command= sys.stdin.readline().split()
+    command = stdin.readline().rstrip().split()
 
     if command[0] == "push":
         stack.append(command[1])
 
-    elif command[0] == "top":
-        if len(stack) == 0:
+    elif command[0] =="pop":
+        if len(stack) == 0 :
             print(-1)
         else:
-            print(stack[-1])
-    elif command[0] =="size":
+            print(stack.pop())
+
+    elif command[0] == "size":
         print(len(stack))
 
-    elif command[0] =="empty":
+    elif command[0] == "empty":
         if len(stack) == 0:
             print(1)
-        else:
-            print(0)
+        else: print(0)
+
     else:
         if len(stack) == 0:
             print(-1)
         else:
-            a= stack.pop()
-            print(a)
+            print(stack[-1])
