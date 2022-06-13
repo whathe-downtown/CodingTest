54057258b793ab5422fd2ea6414fd36e7968a429
Chungkyu Yu <70245821+whathe-downtown@users.noreply.github.com>
2022-06-13 14:32:34 +0900

    [Silver IV] Title: 카드2, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,12 +1,11 @@
-from collections import deque
-n = int(input())
-num_list = deque([x for x in range(1,n+1)])
+input = int(input())
+square = 2
 
-
-while len(num_list) >1:
-    num_list.popleft()
-    a = num_list.popleft()
-    num_list.append(a)
-
-print(num_list[0])
-
+while True:
+    if (input == 1 or input == 2):
+        print(input)
+        break
+    square *= 2
+    if (square >= input):
+        print((input - (square // 2)) * 2)
+        break
