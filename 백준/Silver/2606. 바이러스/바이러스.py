644c169cd2bbf86a0c17f2dc23c4709ccb1d502d
Chungkyu Yu <70245821+whathe-downtown@users.noreply.github.com>
2022-07-15 11:33:41 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,26 @@
+from sys import stdin
+
+input = stdin.readline
+
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+visited = [0] * (n + 1)
+
+
+def dfs(graph, v, visited):
+    visited[v] = 1
+    for i in graph[v]:
+        if visited[i] == 0:
+            dfs(graph, i, visited)
+
+    return True
+
+
+dfs(graph, 1, visited)
+print(sum(visited) - 1)
