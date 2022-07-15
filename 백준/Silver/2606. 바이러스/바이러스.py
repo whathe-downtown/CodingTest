e09c750492f0b8d8cdb1186f96578244475bfa63
Chungkyu Yu <70245821+whathe-downtown@users.noreply.github.com>
2022-07-15 12:03:46 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,26 +1,23 @@
+#DFS
 from sys import stdin
 
-input = stdin.readline
-
-n = int(input())
-m = int(input())
-graph = [[] for _ in range(n + 1)]
+input= stdin.readline
+n= int(input()) #정점Vertax
+m= int(input()) #간선 
+graph =[ [] for _ in range(n+1)] 
 for _ in range(m):
-    x, y = map(int, input().split())
+    x,y = map(int,input().split())
     graph[x].append(y)
     graph[y].append(x)
 
-visited = [0] * (n + 1)
-
-
+visited =[0] * (n+1)
+                    
 def dfs(graph, v, visited):
     visited[v] = 1
     for i in graph[v]:
         if visited[i] == 0:
-            dfs(graph, i, visited)
-
+            dfs(graph,i,visited)
     return True
 
-
-dfs(graph, 1, visited)
-print(sum(visited) - 1)
+dfs (graph,1,visited)
+print(sum(visited)- 1)
